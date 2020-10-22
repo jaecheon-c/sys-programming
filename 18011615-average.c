78589782a605c0ae341d08793b6ea61af03814d0
jaecheon-c <chlwocjs1234@naver.com>
2020-10-21 23:29:54 -0700

    18011615-average.c

--- 18011615-average.c
+++ 18011615-average.c
@@ -3,12 +3,12 @@
 int main(void)
 {
 	int system,algorithm,network;
-	int average;
+	double average;
 	printf("Input the system programming, algorithm, and network score: ");
 	scanf("%d%d%d", &system,&algorithm,&network);
 	
 	average = (system+algorithm+network)/3;
 
-	printf("The average of the three subject is %d.\n",average);
+	printf("The average of the three subject is %d.\n",(int)average);
 	return 0;
 }
